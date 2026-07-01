53f636c555e2d7ede675d297d1c4580621ffb347
vboxuser <vboxuser@Ubuntuu.myguest.virtualbox.org>
2026-07-02 04:00:16 +0530

    Day 1 of Shell Scripting

---
+++ Mango.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Sh : my self S"
+echo "Ta : my self T"
+echo "Sh : will you be my F"
+echo "Ta : umm Y"
+
+FirstName="Shivansh"
+echo " Your first name is $FirstName"
+
+read LastName
+echo " Your Last name is $LastName"
+
+#echo " Your full name is $FirstName $lastName and today is $(date)"
+echo " hiiii $0 $1 $2 "
+
