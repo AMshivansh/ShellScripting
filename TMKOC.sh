09a69da20065ee482e262be0f59be52f55458941
vboxuser <vboxuser@Ubuntuu.myguest.virtualbox.org>
2026-07-02 23:59:08 +0530

    Day 2 conditional, loop, .... ShellScripting

---
+++ TMKOC.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#read -p "Enter bandi name :" bandi
+#if [[ $bandi == "Daya" ]];
+#then 
+#	echo "Loyal Jetha"
+#else
+#	echo "Disloyal"
+#fi
+
+
+user=$1
+if [[ $user == "vboxuse" ]]
+then
+	echo "Haa bhai sahi hai"
+else
+	echo "naa bhai galat hai"
+fi
+
+
